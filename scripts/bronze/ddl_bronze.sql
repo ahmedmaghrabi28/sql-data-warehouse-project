18d2372d9bc1897f0ea751ab67c6bcb8df97ba37
Ahmed Maghrabi <ahmedmaghrabi328@gmail.com>
2026-08-19 19:08:44 +0300

    Rename table from erp_px_cat_giv2 to erp_px_cat_g1v2

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -72,11 +72,11 @@ cntry NVARCHAR(50)
 );
 GO
 
-IF OBJECT_ID ('bronze.erp_px_cat_giv2','U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_giv2;
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE bronze.erp_px_cat_giv2(
+CREATE TABLE bronze.erp_px_cat_g1v2(
 id NVARCHAR(50),
 cat NVARCHAR(50),
 subcat NVARCHAR(50),
